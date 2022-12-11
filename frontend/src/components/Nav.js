23b15722ce4979e00164dd2a8a55eddc0b358e7a
kson4 <davidsinternetmail@gmail.com>
2022-12-11 05:32:54 -0800

    top nav

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './Nav.css'
+
+export default function Nav () {
+  const [showMenu, setShowMenu] = useState(false)
+  return (
+    <nav>
+      <a href='/' className='logo'>
+        Creed Thoughts
+      </a>
+      <div className={showMenu ? 'links' : 'links hidden'}>
+        <ul>
+          <li className='active'>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='/'>About</a>
+          </li>
+          <li>
+            <a href='/'>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <button className={showMenu ? 'btn hidden' : 'btn'}>Login</button>
+      <div id='nav-mobile'>
+        <i
+          className={showMenu ? 'fas fa-times' : 'fas fa-bars'}
+          onClick={() => {
+            console.log('clicked')
+            setShowMenu(!showMenu)
+          }}
+        ></i>
+      </div>
+    </nav>
+  )
+}
